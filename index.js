0397e727b50e12bfaa4866f87dbac4b8bf3d41e6
Javier <sanchezgarciajavi92@gmail.com>
2019-02-18 22:29:28 +0100

    ejercicio

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function addToDo () {
     const li = document.createElement('li');
     const input = document.getElementById('input');
     const text = document.createTextNode(input.value);
+    if(!input.value) {
+        alert ('Introduce una tarea');
+        return;
+    }
     const ul = document.getElementById('list');
     const btnDelete = addDeleteBtnElement();
     const btnEdit = addEditBtnElement();
@@ -14,27 +18,43 @@ function addToDo () {
     ul.appendChild(li);
 }
 function addDeleteBtnElement () {
-    const btnDelete;
+    const btnDelete = document.createElement('button');
+    const text = document.createTextNode('Delete');
+    btnDelete.appendChild(text);
+    btnDelete.addEventListener("click", deleteElement);
     return btnDelete;
 }
 function addEditBtnElement () {
-    const btnEdit;
-
+    const btnEdit = document.createElement('button');
+    const text = document.createTextNode('Edit');
+    btnEdit.appendChild(text);
+    btnEdit.addEventListener("click", editElement);
     return btnEdit;
 }
 function addDoneBtnElement () {
-    const btnDone;
+    const btnDone = document.createElement('button');
+    const text = document.createTextNode('Done');
+    btnDone.appendChild(text);
+    btnDone.addEventListener("click", doneElement);
     return btnDone;
 }
 
 function addSaveBtnElement () {
-    const btnSave;
+    const btnSave = document.createElement('button');
+    const text = document.createTextNode('Save');
+    btnSave.appendChild(text);
+    btnSave.addEventListener("click", saveElement);
     return btnSave;
 }
 function deleteElement(e) {
-    //TODO Borrar el elemento li desde el cual se dispara el evento.
+    const button = this;
+    const li = this.parentNode;
+    const ul = li.parentNode;
+    ul.removeChild(li);
 }
 function doneElement(e) {
+    const li = e.target.parentElement;
+    li.style.backgroundColor = "green";
     //TODO Cambiar el color de fondo del elemento li desde el cual se dispara el evento de rojo a verde.
 }
 function editElement(e) {
@@ -57,6 +77,10 @@ function saveElement(e) {
     const li = e.target.parentElement;
     const input = li.childNodes[0];
     const inputValue = input.value;
+    if(!input.value) {
+        alert ('Introduce un nombre para la tarea');
+        return;
+    }
     const btnSave = li.childNodes[2];
     const btnEdit = addEditBtnElement();
     const text = document.createTextNode(inputValue);
